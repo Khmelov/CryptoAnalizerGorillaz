8e908a48bedc5ca1fa5bc843795f7d8986ce3a4a
akhmelev <akhmelev@gmail.com>
2025-03-04 17:47:28 +0300

    REMOVE module-info.java

--- src/main/java/com/javarush/khmelov/JavaFxRunner.java
+++ src/main/java/com/javarush/khmelov/JavaFxRunner.java
@@ -1,19 +1,9 @@
 package com.javarush.khmelov;
 
-import com.javarush.khmelov.controller.MainController;
-import com.javarush.khmelov.view.javafx.FxForm;
-import javafx.application.Application;
-import javafx.stage.Stage;
+import com.javarush.khmelov.view.javafx.JavaFxApplication;
 
-public class JavaFxRunner extends Application {
+public class JavaFxRunner {
     public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage stage) {
-        MainController controller = new MainController();
-        FxForm fxForm = new FxForm(stage, controller);
-        fxForm.initialization();
+        JavaFxApplication.show(args);
     }
 }
--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.javarush.khmelov {
-    requires javafx.controls; //javafx
-    requires info.picocli;    //picocli
-    requires java.desktop;
-    requires java.logging;    //swing
-    exports com.javarush.khmelov;
-}
